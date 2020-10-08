8fd9f0f48e74673f3e27eed5437efb26af2bb02f
Chema <Chemaruiz94@gmail.com>
2020-10-08 06:07:06 +0200

    commit final_2.2

--- src/Finals.kt
+++ src/Finals.kt
@@ -5,7 +5,6 @@ class Finals {
     companion object{
         const val ENERGIAMAX:Int= 30
         const val FLECHASMAX:Int = 30
-        const val FLEDESPMAX: Int = 10
         const val PODERMALIGNO: Int = 10
         const val ENEMIGOSMAX: Int = 10
         const val HUIR: Int = 80
